cf6a6240e01cc39cbddf02038193596db65a0137
Adrien Cominotto <adc@foyer.lu>
2014-12-17 08:56:26 +0100

    problem 1

--- src/main/scala/problem1.scala
+++ src/main/scala/problem1.scala
@@ -1,6 +1,10 @@
 object MultiplesOf3And5 {
 
-  def apply(): Int = {
-    
+  def sumBelow(limit: Int): Int = {
+    val by3 = 0 until limit by 3
+    val by5 = 0 until limit by 5
+
+    val set = by3.toSet ++ by5.toSet
+    set.sum
   }
 }
--- src/test/scala/problem1Spec.scala
+++ src/test/scala/problem1Spec.scala
@@ -1,6 +1,20 @@
 import org.scalatest._
 
 class MultiplesOf3And5Spec extends FlatSpec with Matchers {
-  "Multiples of 3 And 5" should "have tests" in {
+
+  "sum of multiples of 3 And 5" should "equal 3 for all N below 5" in {
+    MultiplesOf3And5.sumBelow(5) should be(3)
+  }
+
+  it should "equal 23 for all N below 10" in {
+    MultiplesOf3And5.sumBelow(10) should be(23)
+  }
+
+  it should "equal 233168 for all N below 1000" in {
+    MultiplesOf3And5.sumBelow(1000) should be(233168)
+  }
+
+  it should "equal 23331668 for all N below 10000" in {
+    MultiplesOf3And5.sumBelow(10000) should be(23331668)
   }
 }
